0de3d37d3f8ca3d62ea99af7d1bd277b800a38e0
Rudolf Mildner <nanoquack@gmail.com>
2011-12-01 17:45:09 +0100

    notes

--- gui/src/sbc/gui/Main.java
+++ gui/src/sbc/gui/Main.java
@@ -10,22 +10,22 @@ public class Main {
 	}
 
 	protected static void createFrame() {
-//		MainFrame frame = new MainFrame();
-//		frame.setVisible(true);
-		try {
-            Class c = Class.forName("sbc.JmsMain");
-            Object instance=c.newInstance();
-            Method m[] = c.getDeclaredMethods();
-
-            for (int i = 0; i < m.length; i++)
-            System.out.println(m[i].toString());
-            Object[] arglist=new Object[1];
-            arglist[0]=null;
-            m[0].invoke(instance, arglist);
-            
-         }
-         catch (Throwable e) {
-            e.printStackTrace();
-         }
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+//		try {
+//            Class c = Class.forName("sbc.JmsMain");
+//            Object instance=c.newInstance();
+//            Method m[] = c.getDeclaredMethods();
+//
+//            for (int i = 0; i < m.length; i++)
+//            System.out.println(m[i].toString());
+//            Object[] arglist=new Object[1];
+//            arglist[0]=null;
+//            m[0].invoke(instance, arglist);
+//            
+//         }
+//         catch (Throwable e) {
+//            e.printStackTrace();
+//         }
 	}
 }
