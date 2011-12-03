fb0b9e27321ea6490ae50b2e6ad0e04c828703ef
Rudolf Mildner <nanoquack@gmail.com>
2011-12-03 20:29:35 +0100

    xvsm logistics worker implemented

--- xvsm/src/sbc/SbcConstants.java
+++ xvsm/src/sbc/SbcConstants.java
@@ -8,6 +8,8 @@ public class SbcConstants {
 	public static final String TESTERCONTAINER="sbc_tester";
 	public static final String LOGISTICCONTAINER="sbc_logistic";
 	public static final String NOTIFICATIONCONTAINER="sbc_notification";
+	public static final String SALESCONTAINER = "sbc_sales";
+	public static final String RECYCLINGCONTAINER = "sbc_recycling";
 	
 	public static final String ProducerUrl="xvsm://localhost:12345";
 	public static final String NotificationUrl="xvsm://localhost:12345";
@@ -15,6 +17,8 @@ public class SbcConstants {
 //	public static final String ConstructionContainerUrl="xvsm://localhost:12347";
 	public static final String TesterContainerUrl="xvsm://localhost:12347";
 	public static final String LogisticContainerUrl="xvsm://localhost:12348";
+	public static final String SalesContainerUrl = "xvsm://localhost:12349";
+	public static final String RecyclingContainerUrl = "xvsm://localhost:12349";
 	
 	public static final int PRODUCERPORT=12345;
 	public static final int STORAGEPORT=12346;
--- xvsm/src/sbc/xvsm/thread/LogisticsWorker.java
+++ xvsm/src/sbc/xvsm/thread/LogisticsWorker.java
@@ -1,17 +1,22 @@
 package sbc.xvsm.thread;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
 
+import org.mozartspaces.capi3.FifoCoordinator;
 import org.mozartspaces.core.Capi;
 import org.mozartspaces.core.ContainerReference;
 import org.mozartspaces.core.DefaultMzsCore;
 import org.mozartspaces.core.Entry;
+import org.mozartspaces.core.MzsConstants;
 import org.mozartspaces.core.MzsCore;
+import org.mozartspaces.core.MzsCoreRuntimeException;
 import org.slf4j.LoggerFactory;
 
 import sbc.INotifyGui;
 import sbc.SbcConstants;
+import sbc.dto.Computer;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 
@@ -19,21 +24,52 @@ public class LogisticsWorker implements Runnable {
 	private INotifyGui notifyGui;
 	private Capi capi;
 	private MzsCore core;
-	private ContainerReference testContainer;
 	private ContainerReference logisticContainer;
+	private ContainerReference salesContainer;
+	private ContainerReference recyclingContainer;
 	private ContainerReference notficationContainer;
 	private boolean running;
 
 	public static void main(String[] args) {
-		TesterWorker tester = new TesterWorker();
-		Thread t = new Thread(tester);
+		LogisticsWorker logistics = new LogisticsWorker();
+		Thread t = new Thread(logistics);
 		t.start();
 		System.out.println("Logistics Worker started");
 	}
 
+	public LogisticsWorker() {
+		this.running = true;
+	}
+
 	public void run() {
 		try {
-			// Set up local mozart space
+			initXvsm();
+
+			while (running) {
+				List<Computer> entries = capi.take(logisticContainer,
+						Arrays.asList(FifoCoordinator.newSelector()),
+						MzsConstants.RequestTimeout.INFINITE, null);
+				for (Computer computer : entries) {
+					if (computer != null) {
+						if (computer.getQualityCheckPassed()) {
+							capi.write(salesContainer, new Entry(computer));
+							System.out
+									.println("Computer has passed quality check, goes to sales");
+						} else {
+							capi.write(recyclingContainer, new Entry(computer));
+							System.out
+									.println("Computer has NOT passed quality check, goes to recycling");
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initXvsm() {
+		try {
 			LoggerContext context = (LoggerContext) LoggerFactory
 					.getILoggerFactory();
 			JoranConfigurator configurator = new JoranConfigurator();
@@ -41,22 +77,41 @@ public class LogisticsWorker implements Runnable {
 			context.reset();
 			configurator.doConfigure("logback.xml");
 
-			core = DefaultMzsCore.newInstance(SbcConstants.TESTERPORT);
-			capi = new Capi(core);
-
-			this.testContainer = capi.lookupContainer(
-					SbcConstants.LOGISTICCONTAINER, new URI(
-							SbcConstants.LOGISTICCONTAINER), 1000l, null);
-
-			while (running) {
-				List<Entry> entries = capi.take(testContainer);
-				for (Entry entry : entries) {
-					// TODO check if computer is ok and depending on that move
-					// it to sale delivery or recycling storage
-				}
+			try {
+				core = DefaultMzsCore.newInstance(SbcConstants.LOGISTICPORT);
+				capi = new Capi(core);
+				salesContainer = capi.createContainer(
+						SbcConstants.SALESCONTAINER, null,
+						MzsConstants.Container.UNBOUNDED, null,
+						new FifoCoordinator());
+				recyclingContainer = capi.createContainer(
+						SbcConstants.RECYCLINGCONTAINER, null,
+						MzsConstants.Container.UNBOUNDED, null,
+						new FifoCoordinator());
+			} catch (MzsCoreRuntimeException e) {
+				System.err
+						.println("Default port for LogisticsWorker is already taken, using arbitrary port");
+				core = DefaultMzsCore.newInstance(0);
+				capi = new Capi(core);
+				salesContainer = capi.lookupContainer(
+						SbcConstants.SALESCONTAINER, new URI(
+								SbcConstants.SalesContainerUrl),
+						MzsConstants.RequestTimeout.INFINITE, null);
+				recyclingContainer = capi.lookupContainer(
+						SbcConstants.RECYCLINGCONTAINER, new URI(
+								SbcConstants.RecyclingContainerUrl),
+						MzsConstants.RequestTimeout.INFINITE, null);
 			}
+			logisticContainer = capi.lookupContainer(
+					SbcConstants.LOGISTICCONTAINER, new URI(
+							SbcConstants.LogisticContainerUrl),
+					MzsConstants.RequestTimeout.INFINITE, null);
+			notficationContainer = capi.lookupContainer(
+					SbcConstants.NOTIFICATIONCONTAINER, new URI(
+							SbcConstants.NotificationUrl),
+					MzsConstants.RequestTimeout.INFINITE, null);
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.err.println("Could not inintialize Xvsm");
 		}
 	}
 }
